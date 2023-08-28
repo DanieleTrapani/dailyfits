9fe8e41aaa26125a20dcc3c41373740a509adfab
Cristian Goenaga <crisgoera@gmail.com>
2023-08-28 17:58:22 +0200

    API_call method implemented

---
+++ app/services/weather_api.rb
@@ -0,0 +1,5 @@
+class WeatherAPI
+  def self.api_call(long, lat)
+    HTTParty.get("https://api.open-meteo.com/v1/forecast?latitude=#{lat}&longitude=#{long}&daily=weathercode,apparent_temperature_max,apparent_temperature_min,precipitation_sum,windspeed_10m_max&timezone=Europe%2FBerlin")
+  end
+end
