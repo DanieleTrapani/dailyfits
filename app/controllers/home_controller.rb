61d4a68085db3b601789c38f2e37a91e23be6ca2
Cristian Goenaga <crisgoera@gmail.com>
2023-09-04 12:31:26 +0200

    Removed raise in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
       @location = current_user.location
     end
     @weather = WeatherApi.get_day(0, @location)
-    raise
   end
 
   def location_params
