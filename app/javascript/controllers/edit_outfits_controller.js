b429c21261b4f6c9a74c867f79df94e9d758eeb0
Nancy Qian <luyao.q@gmail.com>
2023-09-05 15:07:45 +0200

    edit outfit

--- app/javascript/controllers/edit_outfits_controller.js
+++ app/javascript/controllers/edit_outfits_controller.js
@@ -2,16 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-outfits"
 export default class extends Controller {
-  static targets = ["modifybutton", "buttonlists", "hint"]
+  static targets = ["togglebutton", "buttonlists", "editbutton", "text", "form"]
 
   connect() {
-    // console.log("hello, connect")
+    console.log("hello, connect")
     // console.log(this.modifybuttonTarget)
     // console.log(this.buttonlistsTarget)
   }
 
-  modify() {
+  toggle() {
+    event.preventDefault()
+    console.log("hello, toglle")
     // console.log(this.buttonlistsTarget)
     this.buttonlistsTarget.classList.toggle("d-none")
+  }
+
+  edit(){
+    event.preventDefault()
+    console.log("hello, edit")
+    // console.log(this.textTarget)
+    // console.log(this.formTarget)
+    this.textTarget.classList.add("d-none")
+    this.formTarget.classList.remove("d-none")
   }
 }
