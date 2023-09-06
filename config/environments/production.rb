4c1652e05d2b2b0cc6e8c83cf0cc217d127cf804
Daniele Trapani <daniele.trapani96@gmail.com>
2023-09-06 12:15:32 +0200

    test hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 #   config.hosts << "dailyfits-3a50615e93d9.herokuapp.com"
-  config.hosts.clear
+#   config.hosts.clear
+  config.hosts << "www.dailyfits.me"
 end
