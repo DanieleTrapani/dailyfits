0b53e0fb2271601642897d7596f6476acc3f9a5d
Daniele Trapani <daniele.trapani96@gmail.com>
2023-09-07 10:54:52 +0200

    new seeding, more coherent

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ user = User.create({
 })
 puts "Admin created."
 
-puts "Creating new 5 outfits, outfit2 has multiple photos"
+puts "Creating 5 outfits"
 
-puts "Creating new outfit1"
-file1 = URI.open('https://images.unsplash.com/photo-1521045452978-3753d26aaee4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80')
-outfit1 = Outfit.new(description: "test", user: user)
-outfit1.photos.attach(filename: 'cold.png', io: file1, content_type: 'image/*')
+puts "Creating outfit1"
+file1 = URI.open('https://media-photos.depop.com/b0/19736062/1117052713_d3cc51dde13a4ad3864ebac4f511941c/P8.jpg')
+outfit1 = Outfit.new(description: "green vibes", user: user)
+outfit1.photos.attach(filename: 'outfit1.png', io: file1, content_type: 'image/*')
 today = WeatherApi.get_day(0, user.location)
 tag_params = WeatherApi.create_tag(today)
 tag = Tag.new(tag_params)
@@ -32,14 +32,12 @@ end
 tag.outfits << outfit1
 outfit1.save!
 
-puts "Creating new outfit2"
-file2a = URI.open('https://images.unsplash.com/photo-1600074338311-246e63381568?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')
-file2b = URI.open('https://images.unsplash.com/photo-1600074342811-7764248a2b1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')
-file2c = URI.open('https://images.unsplash.com/photo-1600075110136-e29c6d9e91ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80')
-outfit2 = Outfit.new(description: "test", user: user)
-outfit2.photos.attach(filename: 'cold.png', io: file2a, content_type: 'image/*')
-outfit2.photos.attach(filename: 'cold.png', io: file2b, content_type: 'image/*')
-outfit2.photos.attach(filename: 'cold.png', io: file2c, content_type: 'image/*')
+puts "Creating outfit2"
+file2a = URI.open('https://static.theright.fit/profile/images/52e5b65c-3363-4655-8768-6385c098738a/fnWkDmUUX9fugpmsTuBetnkbTcB0UUDYKk9nFNzp.jpeg')
+file2b = URI.open('https://pbs.twimg.com/media/FxfJYetWwAEwk-N.jpg:large')
+outfit2 = Outfit.new(description: "black outfit variations", user: user)
+outfit2.photos.attach(filename: 'outfit2a.png', io: file2a, content_type: 'image/*')
+outfit2.photos.attach(filename: 'outfit2b.png', io: file2b, content_type: 'image/*')
 today = WeatherApi.get_day(0, user.location)
 tag_params = WeatherApi.create_tag(today)
 tag = Tag.new(tag_params)
@@ -51,10 +49,10 @@ end
 tag.outfits << outfit2
 outfit2.save!
 
-puts "Creating new outfit4"
-file4 = URI.open('https://images.unsplash.com/photo-1621335829175-95f437384d7c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')
-outfit4 = Outfit.new(description: "test", user: user)
-outfit4.photos.attach(filename: 'cold.png', io: file4, content_type: 'image/*')
+puts "Creating outfit3"
+file3 = URI.open('https://i.pinimg.com/736x/c2/54/41/c254412d82dc1ed05a9942b69d9d967e.jpg')
+outfit3 = Outfit.new(description: "test", user: user)
+outfit3.photos.attach(filename: 'outfit3.png', io: file3, content_type: 'image/*')
 today = WeatherApi.get_day(0, user.location)
 tag_params = WeatherApi.create_tag(today)
 tag = Tag.new(tag_params)
@@ -63,13 +61,13 @@ if tag.valid?
 else
   tag = Tag.where(tag_params).first
 end
-tag.outfits << outfit4
-outfit4.save!
+tag.outfits << outfit3
+outfit3.save!
 
 puts "Creating new outfit5"
-file5 = URI.open('https://images.unsplash.com/photo-1607688387752-473f3eec9489?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80')
-outfit5 = Outfit.new(description: "test", user: user)
-outfit5.photos.attach(filename: 'cold.png', io: file5, content_type: 'image/*')
+file5 = URI.open('https://i.pinimg.com/1200x/82/c6/ae/82c6ae274717524864969496873f24be.jpg')
+outfit5 = Outfit.new(description: "evening weat", user: user)
+outfit5.photos.attach(filename: 'outfit5.png', io: file5, content_type: 'image/*')
 today = WeatherApi.get_day(0, user.location)
 tag_params = WeatherApi.create_tag(today)
 tag = Tag.new(tag_params)
@@ -82,9 +80,9 @@ tag.outfits << outfit5
 outfit5.save!
 
 puts "Creating new outfit6"
-file6 = URI.open('https://images.unsplash.com/photo-1576185055363-6d7c88000919?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80')
-outfit6 = Outfit.new(description: "test", user: user)
-outfit6.photos.attach(filename: 'cold.png', io: file6, content_type: 'image/*')
+file6 = URI.open('https://static.theright.fit/profile/images/52e5b65c-3363-4655-8768-6385c098738a/n2KeByqaasnuJJLE0dQ9LZvAHvKFaruUL8YBEEQS.jpeg')
+outfit6 = Outfit.new(description: "smart casual", user: user)
+outfit6.photos.attach(filename: 'outfit6.png', io: file6, content_type: 'image/*')
 today = WeatherApi.get_day(0, user.location)
 tag_params = WeatherApi.create_tag(today)
 tag = Tag.new(tag_params)
