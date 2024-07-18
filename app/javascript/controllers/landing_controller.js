d3b7422051e266bfdf046e5fb19b09e5b26a42d7
Daniele Trapani <daniele.trapani96@gmail.com>
2024-07-18 19:51:44 +0200

    cleaned up some comments

--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -1,34 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="landing"
 export default class extends Controller {
-  static targets = ["image"]
+  static targets = ["image"];
   connect() {
-    // console.log("hello, connect")
-    // console.log(this.imageTarget.src)
-    this.startImageRotation()
+    this.startImageRotation();
   }
 
-  startImageRotation(){
-    console.log("hello, startImageRotation")
+  startImageRotation() {
     const images = [
       "https://res.cloudinary.com/drkv46whi/image/upload/v1694015139/landingimage2_v96xnm.png",
       "https://res.cloudinary.com/drkv46whi/image/upload/v1694015138/landingimage3_hlkntq.png",
       "https://res.cloudinary.com/drkv46whi/image/upload/v1694015138/landingimage4_m8qwge.png",
       "https://res.cloudinary.com/drkv46whi/image/upload/v1694015138/landingimage5_kruqph.png",
-      "https://res.cloudinary.com/drkv46whi/image/upload/v1694015139/landingimage1_qftqns.png"
-    ]
+      "https://res.cloudinary.com/drkv46whi/image/upload/v1694015139/landingimage1_qftqns.png",
+    ];
 
-    let counter = 0
+    let counter = 0;
     setInterval(() => {
-    // console.log(`hello, currentImageUrl ${this.imageTarget.src}`)
-    this.imageTarget.src = images[counter]
-    if(counter == 4) {
-      counter = 0
-    } else {
-      counter += 1
-    }
-    // console.log(`hello, currentImageUrl ${this.imageTarget.src}`)
+      this.imageTarget.src = images[counter];
+      if (counter == 4) {
+        counter = 0;
+      } else {
+        counter += 1;
+      }
     }, 3000);
   }
 }
